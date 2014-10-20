773bd29dbe4e9f7c00d9f18e37f7ce609e866352
banteg <banteeg@gmail.com>
2014-10-20 19:33:48 +0700

    themes view

--- stamps/frontend.py
+++ stamps/frontend.py
@@ -11,4 +11,9 @@ db = mongo.stamps
 @frontend.route('/')
 def index():
     countries = db.stamps.distinct('country')
-    return render_template('index.html', countries=countries)
+    themes = db.stamps.distinct('primary_theme')
+    primary_themes = [t for t in themes if t and not '(' in t]
+
+    return render_template('index.html',
+                           countries=countries,
+                           themes=primary_themes)
