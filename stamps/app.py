09e34df4b419a36a99541efa455f8bebdb0233be
banteg <banteeg@gmail.com>
2014-10-22 21:47:57 +0700

    load mako templates and plim

--- stamps/app.py
+++ stamps/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask.ext.mako import MakoTemplates
+from plim import preprocessor
 
 from stamps.api import api
 from stamps.frontend import frontend
 
 
 app = Flask(__name__)
-app.config.update(JSON_AS_ASCII=False)
+mako = MakoTemplates(app)
+app.config.update(
+    JSON_AS_ASCII=False,
+    MAKO_PREPROCESSOR=preprocessor
+)
 
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(frontend)
