2567566c8d4c5ed128effd87669f483ba8cc2f95
banteg <banteeg@gmail.com>
2014-11-10 23:29:35 +0600

    depend on flask-oauthlib

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stamps',
-    version='0.1.4-dev',
+    version='0.1.5-dev',
     url='https://github.com/banteg/stamps',
 
     packages=['stamps'],
@@ -12,6 +12,7 @@ setup(
         'flask-pymongo',
         'flask-mako',
         'plim',
+        'flask-oauthlib',
     ],
 
     extras_require={
